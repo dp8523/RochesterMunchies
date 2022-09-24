b1e36d34b91d94bcda82090ee7949c6a9c8b8e21
Jaden Seaton <jis6849@rit.edu>
2022-09-23 20:13:12 -0400

    add several java files for further implementation.

---
+++ estore-api/src/main/controller/EstoreController.java
@@ -0,0 +1,11 @@
+import java.util.logging.Logger;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("snacks")
+public class EstoreController {
+    private static final Logger LOG = Logger.getLogger(EstoreController.class.getName());
+     
+}
