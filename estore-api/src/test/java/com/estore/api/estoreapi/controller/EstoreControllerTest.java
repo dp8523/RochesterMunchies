2bc9f60cc157e44d7dbd633aebef279f39abc9be
Robert Huang <rh5661@rit.edu>
2022-10-02 19:58:33 -0400

    controller test

--- estore-api/src/test/java/com/estore/api/estoreapi/controller/EstoreControllerTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/controller/EstoreControllerTest.java
@@ -22,7 +22,7 @@ import org.springframework.http.ResponseEntity;
  * @author SWEN Faculty
  */
 @Tag("Controller-tier")
-public class SnackControllerTest {
+public class EstoreControllerTest {
     private EstoreController snackController;
     private SnackDAO mockSnackDAO;
 
@@ -39,7 +39,7 @@ public class SnackControllerTest {
     @Test
     public void testGetSnack() throws IOException {  // getSnack may throw IOException
         // Setup
-        Snack Snack = new Snack(99,"Galactic Agent");
+        Snack Snack = new Snack(99,"Camel Balls", "Extra Sour Bubble Gum Jawbreaker", 5, 9.99);
         // When the same id is passed in, our mock Snack DAO will return the Snack object
         when(mockSnackDAO.getSnack(Snack.getId())).thenReturn(Snack);
 
@@ -88,7 +88,7 @@ public class SnackControllerTest {
     @Test
     public void testCreateSnack() throws IOException {  // createSnack may throw IOException
         // Setup
-        Snack Snack = new Snack(99,"Wi-Fire");
+        Snack Snack = new Snack(99,"Haribo Coca-Cola Gummies", "Coca-cola flavored gummies in coca-cola bottle shapes", 10, 4.99);
         // when createSnack is called, return true simulating successful
         // creation and save
         when(mockSnackDAO.createSnack(Snack)).thenReturn(Snack);
@@ -104,7 +104,7 @@ public class SnackControllerTest {
     @Test
     public void testCreateSnackFailed() throws IOException {  // createSnack may throw IOException
         // Setup
-        Snack Snack = new Snack(99,"Bolt");
+        Snack Snack = new Snack(99,"Oreos", "Chocolate creme sandwich cookies", 20,  2.99);
         // when createSnack is called, return false simulating failed
         // creation and save
         when(mockSnackDAO.createSnack(Snack)).thenReturn(null);
@@ -119,7 +119,7 @@ public class SnackControllerTest {
     @Test
     public void testCreateSnackHandleException() throws IOException {  // createSnack may throw IOException
         // Setup
-        Snack Snack = new Snack(99,"Ice Gladiator");
+        Snack Snack = new Snack(99,"Animal Crackers", "Crackers shaped like animals", 12, 8.99);
 
         // When createSnack is called on the Mock Snack DAO, throw an IOException
         doThrow(new IOException()).when(mockSnackDAO).createSnack(Snack);
@@ -134,15 +134,15 @@ public class SnackControllerTest {
     @Test
     public void testUpdateSnack() throws IOException { // updateSnack may throw IOException
         // Setup
-        Snack Snack = new Snack(99,"Wi-Fire");
+        Snack Snack = new Snack(99,"Haribo Coca-Cola Gummies", "Coca-cola flavored gummies in coca-cola bottle shapes", 10, 4.99);
         // when updateSnack is called, return true simulating successful
         // update and save
-        when(mockSnackDAO.updateSnack(Snack)).thenReturn(Snack);
-        ResponseEntity<Snack> response = snackController.updateSnack(Snack);
-        Snack.setName("Bolt");
+        when(mockSnackDAO.updateSnack(Snack, "p","5")).thenReturn(Snack);
+        ResponseEntity<Snack> response = snackController.updateSnack(Snack, "p","5");
+        Snack.setName("Oreos");
 
         // Invoke
-        response = snackController.updateSnack(Snack);
+        response = snackController.updateSnack(Snack, "p","5");
 
         // Analyze
         assertEquals(HttpStatus.OK,response.getStatusCode());
@@ -152,13 +152,13 @@ public class SnackControllerTest {
     @Test
     public void testUpdateSnackFailed() throws IOException { // updateSnack may throw IOException
         // Setup
-        Snack Snack = new Snack(99,"Galactic Agent");
+        Snack Snack = new Snack(99,"Camel Balls", "Extra Sour Bubble Gum Jawbreaker", 5, 9.99);
         // when updateSnack is called, return true simulating successful
         // update and save
-        when(mockSnackDAO.updateSnack(Snack)).thenReturn(null);
+        when(mockSnackDAO.updateSnack(Snack, "p","6")).thenReturn(null);
 
         // Invoke
-        ResponseEntity<Snack> response = snackController.updateSnack(Snack);
+        ResponseEntity<Snack> response = snackController.updateSnack(Snack, "p","6");
 
         // Analyze
         assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
@@ -167,12 +167,12 @@ public class SnackControllerTest {
     @Test
     public void testUpdateSnackHandleException() throws IOException { // updateSnack may throw IOException
         // Setup
-        Snack Snack = new Snack(99,"Galactic Agent");
+        Snack Snack = new Snack(99,"Camel Balls", "Extra Sour Bubble Gum Jawbreaker", 5, 9.99);
         // When updateSnack is called on the Mock Snack DAO, throw an IOException
-        doThrow(new IOException()).when(mockSnackDAO).updateSnack(Snack);
+        doThrow(new IOException()).when(mockSnackDAO).updateSnack(Snack, "d","Sour camel balls");
 
         // Invoke
-        ResponseEntity<Snack> response = snackController.updateSnack(Snack);
+        ResponseEntity<Snack> response = snackController.updateSnack(Snack, "d","Sour camel balls");
 
         // Analyze
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
@@ -182,8 +182,8 @@ public class SnackControllerTest {
     public void testGetEstore() throws IOException { // getestore may throw IOException
         // Setup
         Snack[] estore = new Snack[2];
-        estore[0] = new Snack(99,"Bolt");
-        estore[1] = new Snack(100,"The Great Iguana");
+        estore[0] = new Snack(99,"Oreos", "Chocolate creme sandwich cookies", 20,  2.99);
+        estore[1] = new Snack(100,"Twix","Chocolate covered, caramel filled, cookie sticks",50,2.99);
         // When getestore is called return the estore created above
         when(mockSnackDAO.getSnacks()).thenReturn(estore);
 
@@ -213,8 +213,8 @@ public class SnackControllerTest {
         // Setup
         String searchString = "la";
         Snack[] estore = new Snack[2];
-        estore[0] = new Snack(99,"Galactic Agent");
-        estore[1] = new Snack(100,"Ice Gladiator");
+        estore[0] = new Snack(99,"Camel Balls", "Extra Sour Bubble Gum Jawbreaker", 5, 9.99);
+        estore[1] = new Snack(100,"Animal Crackers", "Crackers shaped like animals", 12, 8.99);
         // When findestore is called with the search string, return the two
         /// estore above
         when(mockSnackDAO.findSnacks(searchString)).thenReturn(estore);
