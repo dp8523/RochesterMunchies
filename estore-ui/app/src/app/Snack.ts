a78cd295d296c82066b77dc4cef57cd22145c8ac
Jaden Seaton <jis6849@rit.edu>
2022-10-06 10:29:28 -0400

    started to create the ui

---
+++ estore-ui/app/src/app/Snack.ts
@@ -0,0 +1,5 @@
+export interface Snack{
+    id: number;
+    name: string;
+    description: string;
+}
---
+++ estore-ui/app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InventoryComponent } from './inventory/inventory.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InventoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
