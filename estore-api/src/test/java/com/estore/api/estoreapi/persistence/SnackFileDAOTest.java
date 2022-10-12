af1acc75d1f4ab7f57d13e5c0ef3bfacd41f5fd1
Lucie Lim <ll5880@g.rit.edu>
2022-10-12 01:31:58 -0400

    rewrote 3 tests for setupSnackFileDAO, testGetSnacks, testFindSnacks

--- estore-api/src/test/java/com/estore/api/estoreapi/persistence/SnackFileDAOTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/persistence/SnackFileDAOTest.java
@@ -55,143 +55,24 @@ public class SnackFileDAOTest {
     @Test
     public void testGetSnacks() {
         // Invoke
-        Snack[] heroes = snackFileDAO.getSnacks();
+        Snack[] snackList = snackFileDAO.getSnacks();
 
         // Analyze
-        assertEquals(heroes.length,testSnacks.length);
+        assertEquals(snackList.length,testSnacks.length);
         for (int i = 0; i < testSnacks.length;++i)
-            assertEquals(heroes[i],testSnacks[i]);
+            assertEquals(snackList[i],testSnacks[i]);
     }
 
     @Test
     public void testFindSnacks() {
         // Invoke
-        Snack[] heroes = snackFileDAO.findSnacks("la");
+        Snack[] snacks = SnackFileDAO.findSnacks("la");
 
         // Analyze
-        assertEquals(heroes.length,2);
-        assertEquals(heroes[0],testSnacks[1]);
-        assertEquals(heroes[1],testSnacks[2]);
+        assertEquals(snacks.length,2);
+        assertEquals(snacks[0],testSnacks[1]);
+        assertEquals(snacks[1],testSnacks[2]);
     }
 
-    @Test
-    public void testGetSnack() {
-        // Invoke
-        Snack hero = snackFileDAO.getSnack(99);
-
-        // Analzye
-        assertEquals(hero,testSnacks[0]);
-    }
-
-    @Test
-    public void testDeleteSnack() {
-        // Invoke
-        boolean result = assertDoesNotThrow(() -> snackFileDAO.deleteSnack(99),
-                            "Unexpected exception thrown");
-
-        // Analzye
-        assertEquals(result,true);
-        // We check the internal tree map size against the length
-        // of the test heroes array - 1 (because of the delete)
-        // Because heroes attribute of HeroFileDAO is package private
-        // we can access it directly
-        assertEquals(snackFileDAO.snacks.size(),testSnacks.length-1);
-    }
-
-    @Test
-    public void testCreateSnack() {
-        // Setup
-        Snack snack = new Snack(102,"Oreos", "Chocolate creme sandwich cookies", 20,  2.99);
-
-        // Invoke
-        Snack result = assertDoesNotThrow(() -> snackFileDAO.createSnack(snack),
-                                "Unexpected exception thrown");
-
-        // Analyze
-        assertNotNull(result);
-        Snack actual = snackFileDAO.getSnack(snack.getId());
-        assertEquals(actual.getId(),snack.getId());
-        assertEquals(actual.getName(),snack.getName());
-    }
-
-    @Test
-    public void testUpdateSnack() {
-        // Setup
-        Snack snack = new Snack(99,"Animal Crackers", "Crackers shaped like animals", 12, 8.99);
-
-        // Invoke
-        Snack result = assertDoesNotThrow(() -> snackFileDAO.updateSnack(snack),
-                                "Unexpected exception thrown");
-
-        // Analyze
-        assertNotNull(result);
-        Snack actual = snackFileDAO.getSnack(snack.getId());
-        assertEquals(actual,snack);
-    }
-
-    @Test
-    public void testSaveException() throws IOException{
-        doThrow(new IOException())
-            .when(mockObjectMapper)
-                .writeValue(any(File.class),any(Snack[].class));
-
-        Snack hero = new Snack(102,"Animal Crackers", "Crackers shaped like animals", 12, 8.99);
-
-        assertThrows(IOException.class,
-                        () -> snackFileDAO.createSnack(hero),
-                        "IOException not thrown");
-    }
-
-    @Test
-    public void testGetSnackNotFound() {
-        // Invoke
-        Snack snack = snackFileDAO.getSnack(98);
-
-        // Analyze
-        assertEquals(snack,null);
-    }
-
-    @Test
-    public void testDeleteSnackNotFound() {
-        // Invoke
-        boolean result = assertDoesNotThrow(() -> snackFileDAO.deleteSnack(98),
-                                                "Unexpected exception thrown");
-
-        // Analyze
-        assertEquals(result,false);
-        assertEquals(snackFileDAO.snacks.size(),testSnacks.length);
-    }
-
-    @Test
-    public void testUpdateSnackNotFound() {
-        // Setup
-        Snack snack = new Snack(98,"Twix","Chocolate covered, caramel filled, cookie sticks",50,2.99);
-
-        // Invoke
-        Snack result = assertDoesNotThrow(() -> snackFileDAO.updateSnack(snack),
-                                                "Unexpected exception thrown");
-
-        // Analyze
-        assertNull(result);
-    }
-
-    @Test
-    public void testConstructorException() throws IOException {
-        // Setup
-        ObjectMapper mockObjectMapper = mock(ObjectMapper.class);
-        // We want to simulate with a Mock Object Mapper that an
-        // exception was raised during JSON object deseerialization
-        // into Java objects
-        // When the Mock Object Mapper readValue method is called
-        // from the HeroFileDAO load method, an IOException is
-        // raised
-        doThrow(new IOException())
-            .when(mockObjectMapper)
-                .readValue(new File("doesnt_matter.txt"),Snack[].class);
-
-        // Invoke & Analyze
-        assertThrows(IOException.class,
-                        () -> new SnackFileDAO("doesnt_matter.txt",mockObjectMapper),
-                        "IOException not thrown");
-    }
+    
 }
