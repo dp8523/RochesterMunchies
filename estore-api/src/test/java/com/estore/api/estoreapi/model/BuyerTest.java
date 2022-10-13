283ca79b373d5456cdc67fd164c8a3a441585d2b
dp8523 <dp8523@g.rit.edu>
2022-10-12 23:37:02 -0400

    Created BuyerTest for current state of Buyer class

---
+++ estore-api/src/test/java/com/estore/api/estoreapi/model/BuyerTest.java
@@ -0,0 +1,57 @@
+package com.estore.api.estoreapi.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.mockito.Mockito.description;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The unit test suite for Buyer
+ * 
+ * @author Dara Prak
+ */
+@Tag("Model-tier")
+public class BuyerTest {
+    @Test
+    public void testCtor() {
+        // Setup
+        String expectedUsername = "SnackLover";
+
+        // Invoke
+        Buyer buyer = new Buyer(expectedUsername);
+
+        // Analyze
+        assertEquals(expectedUsername,buyer.getName());
+    }
+
+    @Test
+    public void testName() {
+        // Setup
+        String username = "SnackLover";
+        Buyer buyer = new Buyer(username);
+
+        String expectedUsername = "SnackEnjoyer";
+
+        // Invoke
+        buyer.setName(expectedUsername);
+
+        // Analyze
+        assertEquals(expectedUsername,buyer.getName());
+    }
+
+    @Test
+    public void testToString() {
+        // Setup
+        String username = "SnackLover";
+
+        String expectedString = String.format(Buyer.STRING_FORMAT,username);
+        Buyer buyer = new Buyer(username);
+
+        // Invoke
+        String actualString = buyer.toString();
+
+        // Analyze
+        assertEquals(expectedString,actualString);
+    }
+}
