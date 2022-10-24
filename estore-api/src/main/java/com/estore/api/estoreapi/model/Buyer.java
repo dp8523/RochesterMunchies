377800e9aa5cb12bb7d7d2bdabba9dc2318bf12a
Adam Pang <akp4339@rit.edu>
2022-10-24 15:15:53 -0400

    Uncommented hashcode and equals

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Buyer.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Buyer.java
@@ -58,31 +58,31 @@ public class Buyer {
     /**
      * {@inheritDoc}
      */
-    // @Override
-    // public int hashCode() {
-    //     final int prime = 31;
-    //     int result = 1;
-    //     result = prime * result + ((username == null) ? 0 : username.hashCode());
-    //     return result;
-    // }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
 
-    // /**
-    //  * {@inheritDoc}
-    //  */
-    // @Override
-    // public boolean equals(Object obj) {
-    //     if (this == obj)
-    //         return true;
-    //     if (obj == null)
-    //         return false;
-    //     if (getClass() != obj.getClass())
-    //         return false;
-    //     Buyer other = (Buyer) obj;
-    //     if (username == null) {
-    //         if (other.username != null)
-    //             return false;
-    //     } else if (!username.equals(other.username))
-    //         return false;
-    //     return true;
-    // }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Buyer other = (Buyer) obj;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
+    }
 }
