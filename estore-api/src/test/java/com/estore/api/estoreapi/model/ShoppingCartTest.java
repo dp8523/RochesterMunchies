d861234642ba6b91738b456dc16a0ae668ac2035
dp8523 <dp8523@g.rit.edu>
2022-11-02 17:55:56 -0400

    began writing ShoppingCartTest

---
+++ estore-api/src/test/java/com/estore/api/estoreapi/model/ShoppingCartTest.java
@@ -0,0 +1,38 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// // import static org.mockito.Mockito.description;
+
+// import org.junit.jupiter.api.Tag;
+// import org.junit.jupiter.api.Test;
+
+// /**
+//  * The unit test suite for ShoppingCart
+//  * 
+//  * @author Dara Prak
+//  */
+// @Tag("Model-tier")
+// public class ShoppingCartTest {
+
+//     @Test
+//     public void testAddToCart() {
+        
+//     }
+
+//     @Test
+//     public void testToString() {
+//         // Setup
+//         ShoppingCart shoppingCart = new ShoppingCart();
+//         shoppingCart.addItemToCart(10);
+//         shoppingCart.addItemToCart(10);
+//         shoppingCart.addItemToCart(11);
+
+//         System.out.println(shoppingCart.toString());
+
+//         // String expectedString = "";
+
+//         // // Invoke
+//         // String actualString = shoppingCart.toString();
+
+//         // // Analyze
+//         // assertEquals(expectedString,actualString);
+//     }
+// }
