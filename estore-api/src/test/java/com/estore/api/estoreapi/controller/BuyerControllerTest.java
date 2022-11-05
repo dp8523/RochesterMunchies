4a70996a3cf05b6e58ff1ebf0254aad678b494d8
Adam Pang <akp4339@rit.edu>
2022-11-04 20:00:18 -0400

    Completed default BuyerController tests

--- estore-api/src/test/java/com/estore/api/estoreapi/controller/BuyerControllerTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/controller/BuyerControllerTest.java
@@ -122,170 +122,60 @@ public class BuyerControllerTest {
     }
 
     
-//     @Test
-//     public void testCreateSnackHandleException() throws IOException {  // createSnack may throw IOException
-//         // Setup
-//         Snack Snack = new Snack(99,"Animal Crackers", "Crackers shaped like animals", 12, 8.99);
+    @Test
+    public void testCreateBuyerHandleException() throws IOException {  // createSnack may throw IOException
+        // Setup
+        String username = "Sudhir";
 
-//         // When createSnack is called on the Mock Snack DAO, throw an IOException
-//         doThrow(new IOException()).when(mockSnackDAO).createSnack(Snack);
+        // When createSnack is called on the Mock Snack DAO, throw an IOException
+        doThrow(new IOException()).when(mockBuyerDAO).createBuyer(username);
 
-//         // Invoke
-//         ResponseEntity<Snack> response = snackController.createSnack(Snack);
+        // Invoke
+        ResponseEntity<Buyer> response = buyerController.createBuyer(username);
 
-//         // Analyze
-//         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
-//     }
+        // Analyze
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
+    }
 
-//     @Test
-//     public void testUpdateSnack() throws IOException { // updateSnack may throw IOException
-//         // Setup
-//         Snack Snack = new Snack(12,"Haribo Coca-Cola Gummies", "Coca-cola flavored gummies in coca-cola bottle shapes", 10, 4.99);
-//         // when updateSnack is called, return true simulating successful
-//         // update and save
-//         when(mockSnackDAO.updateSnack(Snack)).thenReturn(Snack);
-//         ResponseEntity<Snack> response = snackController.updateSnack(Snack);
-//         Snack.setName("Oreos");
+    @Test
+    public void testDeleteBuyer() throws IOException { // deleteSnack may throw IOException
+        // Setup
+        String username = "Sudhir";
+        // when deleteSnack is called return true, simulating successful deletion
+        when(mockBuyerDAO.deleteBuyer(username)).thenReturn(true);
 
-//         // Invoke
-//         response = snackController.updateSnack(Snack);
+        // Invoke
+        ResponseEntity<Buyer> response = buyerController.deleteBuyer(username);
 
-//         // Analyze
-//         assertEquals(HttpStatus.OK,response.getStatusCode());
-//         assertEquals(Snack,response.getBody());
-//     }
+        // Analyze
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+    }
 
-//     @Test
-//     public void testUpdateSnackFailed() throws IOException { // updateSnack may throw IOException
-//         // Setup
-//         Snack Snack = new Snack(99,"Camel Balls", "Extra Sour Bubble Gum Jawbreaker", 5, 9.99);
-//         // when updateSnack is called, return true simulating successful
-//         // update and save
-//         when(mockSnackDAO.updateSnack(Snack)).thenReturn(null);
+    @Test
+    public void testDeleteBuyerNotFound() throws IOException { // deleteSnack may throw IOException
+        // Setup
+        String username = "Sudhir";
+        // when deleteSnack is called return false, simulating failed deletion
+        when(mockBuyerDAO.deleteBuyer(username)).thenReturn(false);
 
-//         // Invoke
-//         ResponseEntity<Snack> response = snackController.updateSnack(Snack);
+        // Invoke
+        ResponseEntity<Buyer> response = buyerController.deleteBuyer(username);
 
-//         // Analyze
-//         assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
-//     }
+        // Analyze
+        assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
+    }
 
-//     @Test
-//     public void testUpdateSnackHandleException() throws IOException { // updateSnack may throw IOException
-//         // Setup
-//         Snack Snack = new Snack(99,"Camel Balls", "Extra Sour Bubble Gum Jawbreaker", 5, 9.99);
-//         // When updateSnack is called on the Mock Snack DAO, throw an IOException
-//         doThrow(new IOException()).when(mockSnackDAO).updateSnack(Snack);
+    @Test
+    public void testDeleteBuyerHandleException() throws IOException { // deleteSnack may throw IOException
+        // Setup
+        String username = "Sudhir";
+        // When deleteSnack is called on the Mock Snack DAO, throw an IOException
+        doThrow(new IOException()).when(mockBuyerDAO).deleteBuyer(username);
 
-//         // Invoke
-//         ResponseEntity<Snack> response = snackController.updateSnack(Snack);
+        // Invoke
+        ResponseEntity<Buyer> response = buyerController.deleteBuyer(username);
 
-//         // Analyze
-//         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
-//     }
-
-//     @Test
-//     public void testGetSnacks() throws IOException { // getSnacks may throw IOException
-//         // Setup
-//         Snack[] snacks = new Snack[2];
-//         snacks[0] = new Snack(99,"Oreos", "Chocolate creme sandwich cookies", 20,  2.99);
-//         snacks[1] = new Snack(100,"Twix","Chocolate covered, caramel filled, cookie sticks",50,2.99);
-//         // When getSnacks is called return the snacks created above
-//         when(mockSnackDAO.getSnacks()).thenReturn(snacks);
-
-//         // Invoke
-//         ResponseEntity<Snack[]> response = snackController.getSnacks();
-
-//         // Analyze
-//         assertEquals(HttpStatus.OK,response.getStatusCode());
-//         assertEquals(snacks,response.getBody());
-//     }
-
-//     @Test
-//     public void testGetSnacksHandleException() throws IOException { // getSnacks may throw IOException
-//         // Setup
-//         // When getSnacks is called on the Mock Snack DAO, throw an IOException
-//         doThrow(new IOException()).when(mockSnackDAO).getSnacks();
-
-//         // Invoke
-//         ResponseEntity<Snack[]> response = snackController.getSnacks();
-
-//         // Analyze
-//         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
-//     }
-
-//     @Test
-//     public void testSearchSnacks() throws IOException { // findSnacks may throw IOException
-//         // Setup
-//         String searchString = "la";
-//         Snack[] snacks = new Snack[2];
-//         snacks[0] = new Snack(99,"Camel Balls", "Extra Sour Bubble Gum Jawbreaker", 5, 9.99);
-//         snacks[1] = new Snack(100,"Animal Crackers", "Crackers shaped like animals", 12, 8.99);
-//         // When findSnacks is called with the search string, return the two
-//         /// snacks above
-//         when(mockSnackDAO.findSnacks(searchString)).thenReturn(snacks);
-
-//         // Invoke
-//         ResponseEntity<Snack[]> response = snackController.searchSnacks(searchString);
-
-//         // Analyze
-//         assertEquals(HttpStatus.OK,response.getStatusCode());
-//         assertEquals(snacks,response.getBody());
-//     }
-
-//     @Test
-//     public void testSearchSnacksHandleException() throws IOException { // findSnacks may throw IOException
-//         // Setup
-//         String searchString = "an";
-//         // When createSnack is called on the Mock Snack DAO, throw an IOException
-//         doThrow(new IOException()).when(mockSnackDAO).findSnacks(searchString);
-
-//         // Invoke
-//         ResponseEntity<Snack[]> response = snackController.searchSnacks(searchString);
-
-//         // Analyze
-//         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
-//     }
-
-//     @Test
-//     public void testDeleteSnack() throws IOException { // deleteSnack may throw IOException
-//         // Setup
-//         int SnackId = 99;
-//         // when deleteSnack is called return true, simulating successful deletion
-//         when(mockSnackDAO.deleteSnack(SnackId)).thenReturn(true);
-
-//         // Invoke
-//         ResponseEntity<Snack> response = snackController.deleteSnack(SnackId);
-
-//         // Analyze
-//         assertEquals(HttpStatus.OK,response.getStatusCode());
-//     }
-
-//     @Test
-//     public void testDeleteSnackNotFound() throws IOException { // deleteSnack may throw IOException
-//         // Setup
-//         int SnackId = 99;
-//         // when deleteSnack is called return false, simulating failed deletion
-//         when(mockSnackDAO.deleteSnack(SnackId)).thenReturn(false);
-
-//         // Invoke
-//         ResponseEntity<Snack> response = snackController.deleteSnack(SnackId);
-
-//         // Analyze
-//         assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
-//     }
-
-//     @Test
-//     public void testDeleteSnackHandleException() throws IOException { // deleteSnack may throw IOException
-//         // Setup
-//         int SnackId = 99;
-//         // When deleteSnack is called on the Mock Snack DAO, throw an IOException
-//         doThrow(new IOException()).when(mockSnackDAO).deleteSnack(SnackId);
-
-//         // Invoke
-//         ResponseEntity<Snack> response = snackController.deleteSnack(SnackId);
-
-//         // Analyze
-//         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
-//     }
+        // Analyze
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
+    }
 }
