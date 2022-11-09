a986331452b2b7bc432347a170a171e593dfb9a8
Adam Pang <akp4339@rit.edu>
2022-11-08 19:08:55 -0500

    Began ShoppingCartTest unit tests

--- estore-api/src/test/java/com/estore/api/estoreapi/model/ShoppingCartTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/model/ShoppingCartTest.java
@@ -1,38 +1,66 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// // import static org.mockito.Mockito.description;
+package com.estore.api.estoreapi.model;
 
-// import org.junit.jupiter.api.Tag;
-// import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// /**
-//  * The unit test suite for ShoppingCart
-//  * 
-//  * @author Dara Prak
-//  */
-// @Tag("Model-tier")
-// public class ShoppingCartTest {
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
-//     @Test
-//     public void testAddToCart() {
-        
-//     }
+/**
+ * The unit test suite for ShoppingCart
+ */
+@Tag("Model-tier")
+public class ShoppingCartTest {
+    @Test
+    public void testCtor() {
+        // Setup
+        int expected_id = 99;
+        String expected_name = "Camel Balls";
+        String expected_description = "Extra Sour Bubble Gum Jawbreaker";
+        int expected_quantity = 5;
+        double expected_price = 9.99;
 
-//     @Test
-//     public void testToString() {
-//         // Setup
-//         ShoppingCart shoppingCart = new ShoppingCart();
-//         shoppingCart.addItemToCart(10);
-//         shoppingCart.addItemToCart(10);
-//         shoppingCart.addItemToCart(11);
+        // Invoke
+        Snack snack = new Snack(expected_id,expected_name,expected_description,expected_quantity,expected_price);
 
-//         System.out.println(shoppingCart.toString());
+        // Analyze
+        assertEquals(expected_id,snack.getId());
+        assertEquals(expected_name,snack.getName());
+    }
 
-//         // String expectedString = "";
+    // @Test
+    // public void testName() {
+    //     // Setup
+    //     int id = 99;
+    //     String name = "Camel Balls";
+    //     String description = "Extra Sour Bubble Gum Jawbreaker";
+    //     int quantity = 5;
+    //     double price = 9.99;
+    //     Snack snack = new Snack(id,name,description,quantity,price);
 
-//         // // Invoke
-//         // String actualString = shoppingCart.toString();
+    //     String expected_name = "Animal Crackers";
 
-//         // // Analyze
-//         // assertEquals(expectedString,actualString);
-//     }
-// }
+    //     // Invoke
+    //     snack.setName(expected_name);
+
+    //     // Analyze
+    //     assertEquals(expected_name,snack.getName());
+    // }
+
+    // @Test
+    // public void testToString() {
+    //     // Setup
+    //     int id = 99;
+    //     String name = "Camel Balls";
+    //     String description = "Extra Sour Bubble Gum Jawbreaker";
+    //     int quantity = 5;
+    //     double price = 9.99;
+    //     String expected_string = String.format(Snack.STRING_FORMAT,id,name,description,quantity,price);
+    //     Snack snack = new Snack(id,name,description,quantity,price);
+
+    //     // Invoke
+    //     String actual_string = snack.toString();
+
+    //     // Analyze
+    //     assertEquals(expected_string,actual_string);
+    // }
+}
